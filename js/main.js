6b4298f92ea02ff373ec9a2787c01afa3466f238
juhuiguang <juhuiguang@foxmail.com>
2017-02-01 01:53:34 +0800

    初始化导入

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 $(function(){
-
+    console.log("又是一年迎财神，前30年都没有遇上，希望今年财神赏个脸！");
+    console.log("爆料，此网站一边迎着财神一边开发的。");
     fullscreen();
     var mySwiper = new Swiper ('.swiper-container', {
         direction: 'vertical',
         loop: true,
         effect: 'fade',
+        direction : 'vertical',
         autoplay:true,
         speed:3000,
         // 如果需要分页器
@@ -27,8 +29,10 @@ $(function(){
         var height=$(window).height();
         //竖屏的情况
         if(height>width){
+            //var rate=height/1080*100;
             $(".fullscreen").width(width).height(height).css("background-size","auto 100%");
         }else{
+            //var rate=width/1920*100;
             $(".fullscreen").width(width).height(height).css("background-size","100% auto");
         }
     }
@@ -39,4 +43,8 @@ $(function(){
             $(".welcomgroup").remove();
         });
     }
+
+    $("#togglebtn").click(function(){
+        $("#mobilenavlist").toggle();
+    });
 });
